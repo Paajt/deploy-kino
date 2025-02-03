e85d4a88e020b141480e20f9f304bf6f6bc1c88c
Alfred <alfred-brannarer@hotmail.com>
2025-02-03 08:53:14 +0100

    Corrected movie reviews data extraction in getAverageRating

--- src/routes/getAverageRating.js
+++ src/routes/getAverageRating.js
@@ -1,11 +1,11 @@
 export default async function getAverageRating(cmsAdapter, movieId) {
   try {
-    const movieReviews = await cmsAdapter.fetchAverageRating(movieId);
-    const validReviews = movieReviews.filter((review) => review.attributes.rating > 0);
+    const movieReviewsResponse = await cmsAdapter.fetchAverageRating(movieId);
+    const movieReviews = movieReviewsResponse.data;
 
-    if (validReviews.length >= 5) {
-      const totalRating = validReviews.reduce((sum, review) => sum + review.attributes.rating, 0);
-      const averageRating = totalRating / validReviews.length;
+    if (movieReviews.length >= 5) {
+      const totalRating = movieReviews.reduce((sum, review) => sum + review.attributes.rating, 0);
+      const averageRating = totalRating / movieReviews.length;
       return averageRating;
     } else {
       const imdbID = await cmsAdapter.fetchIMDBID(movieId);
