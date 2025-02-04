c7521b027304b922610f943b0264672aa17f0202
Alfred <alfred-brannarer@hotmail.com>
2025-02-04 17:10:47 +0100

    Updated console.log when not on movie page

--- src/main.js
+++ src/main.js
@@ -30,7 +30,7 @@ if (document.querySelector('.reviews__container')) {
   const movieAvRating = new AverageRating(avRatingBackend);
   movieAvRating.renderAvRating(document.querySelector('.averageRating__container'));
 } else {
-  console.log('No reviews container found.');
+  console.log('Not on a movie page, skipping reviews.');
 }
 
 const review = document.querySelector('.review');
