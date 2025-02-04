e5d477dcb482a1b4c85243932198b6e9f3943f38
Patrik <pat_p3@hotmail.com>
2025-02-04 08:54:11 +0100

    Updated route for depencendy injection use

--- src/routes/topMoviesRoute.js
+++ src/routes/topMoviesRoute.js
@@ -1,17 +1,19 @@
 import express from 'express';
-import { getTopMovies } from '../../lib/topMovies.js';
 
 const router = express.Router();
 
-// API-endpoint to fetch most popular movies
-router.get('/top-movies', async (req, res) => {
-  try {
-    const topMovies = await getTopMovies();
-    res.json(topMovies);
-  } catch (error) {
-    console.error('Error getting top rated movies:', error);
-    res.status(500).json({ error: 'Could not get top rated movies' });
-  }
-});
+function createTopMoviesRoute(getTopMovies) {
+  // API-endpoint to fetch most popular movies
+  router.get('/top-movies', async (req, res) => {
+    try {
+      const topMovies = await getTopMovies();
+      res.json(topMovies);
+    } catch (error) {
+      console.error('Error getting top rated movies:', error);
+      res.status(500).json({ error: 'Could not get top rated movies' });
+    }
+  });
+  return router;
+}
 
-export default router;
+export default createTopMoviesRoute;
