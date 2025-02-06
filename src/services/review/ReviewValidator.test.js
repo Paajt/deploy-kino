d3b4878cf1456bff5c0535b3b406c787e6336f10
Kirill <kirillsavorin@gmail.com>
2025-02-06 17:38:55 +0100

    Prev Tests update

--- src/services/review/ReviewValidator.test.js
+++ src/services/review/ReviewValidator.test.js
@@ -1,15 +1,24 @@
 import { ReviewValidator } from './ReviewValidator';
 
 describe('ReviewValidator', () => {
-  test('should throw error for missing rating', () => {
+  test('Should throw error for missing rating', () => {
+    const author = { authBtnClick: true, isLoggedIn: true };
     expect(() => {
-      ReviewValidator.validate('Great movie!', null);
+      ReviewValidator.validate('Great movie!', null, author);
     }).toThrow('Please select a rating');
   });
 
-  test('should throw error for empty comment', () => {
+  test('Should throw error for empty comment', () => {
+    const author = { authBtnClick: true, isLoggedIn: true };
     expect(() => {
-      ReviewValidator.validate('', 5);
+      ReviewValidator.validate('', 5, author);
     }).toThrow('Please write a review');
+  });
+
+  test('Should throw error for wrong password', () => {
+    const author = { authBtnClick: false, isLoggedIn: false };
+    expect(() => {
+      ReviewValidator.validate('Great movie!', 5, author);
+    }).toThrow('Wrong password');
   });
 });
